63a39795029b11b5341fb33467d65075c879ba04
alessio <alessio.peluso@agilelab.it>
2023-07-07 09:50:14 +0200

    modified config.js

--- docusaurus-test/docusaurus.config.js
+++ docusaurus-test/docusaurus.config.js
@@ -11,15 +11,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://AlessioPeluso.github.io/docusaurus-test/',
+  url: 'https://AlessioPeluso.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docusaurus-test',
 
+  // deploymentBranch: 'main',
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'AlessioPeluso', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'docusaurus-test', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
